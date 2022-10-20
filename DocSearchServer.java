7363f2a1f1823e24e6228f12f8cc16f34c9d1cec
Michael Owens <33632547+why-does-ie-still-exist@users.noreply.github.com>
2022-10-20 11:26:23 -0700

    Format files, add test file, add run scripts

--- DocSearchServer.java
+++ DocSearchServer.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,47 +8,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 class FileHelpers {
-    static List<File> getFiles(Path start) throws IOException {
-        File f = start.toFile();
-        List<File> result = new ArrayList<>();
-        if(f.isDirectory()) {
-            System.out.println("It's a folder");
-            File[] paths = f.listFiles();
-            for(File subFile: paths) {
-                result.addAll(getFiles(subFile.toPath()));
-            }
-        }
-        else {
-            result.add(start.toFile());
-        }
-        return result;
+  static List<File> getFiles(Path start) throws IOException {
+    File f = start.toFile();
+    List<File> result = new ArrayList<>();
+    if (f.isDirectory()) {
+      System.out.println("It's a folder");
+      File[] paths = f.listFiles();
+      for (File subFile : paths) {
+        result.addAll(getFiles(subFile.toPath()));
+      }
+    } else {
+      result.add(start.toFile());
     }
-    static String readFile(File f) throws IOException {
-        System.out.println(f.toString());
-        return new String(Files.readAllBytes(f.toPath()));
-    }
+    return result;
+  }
+
+  static String readFile(File f) throws IOException {
+    System.out.println(f.toString());
+    return new String(Files.readAllBytes(f.toPath()));
+  }
 }
 
 class Handler implements URLHandler {
-    List<File> files;
-    Handler(String directory) throws IOException {
-      this.files = FileHelpers.getFiles(Paths.get(directory));
+  List<File> files;
+
+  Handler(String directory) throws IOException {
+    this.files = FileHelpers.getFiles(Paths.get(directory));
+  }
+
+  public String handleRequest(URI url) throws IOException {
+    if (url.getPath().equals("/")) {
+      return "There are " + files.size() + " files to search";
+    } else if (url.getPath().equalsIgnoreCase("/search")) {
+
     }
-    public String handleRequest(URI url) throws IOException {
-      return "Don't know how to handle that path!";
-    }
+    return "404 not found";
+  }
 }
 
 class DocSearchServer {
-    public static void main(String[] args) throws IOException {
-        if(args.length == 0){
-            System.out.println("Missing port number! Try any number between 1024 to 49151");
-            return;
-        }
-
-        int port = Integer.parseInt(args[0]);
-
-        Server.start(port, new Handler("./technical/"));
+  public static void main(String[] args) throws IOException {
+    if (args.length == 0) {
+      System.out.println("Missing port number! Try any number between 1024 to 49151");
+      return;
     }
-}
 
+    int port = Integer.parseInt(args[0]);
+
+    Server.start(port, new Handler("./technical/"));
+  }
+}
---
+++ DocSearchTest.java
@@ -0,0 +1,6 @@
+import org.junit.Test;
+
+public class DocSearchTest {
+  @Test
+  public void testHandler() {}
+}
